22b1aa236377fe28427e67ccf83fb6a8596bfdc7
Alexander Okbai <alexokbai1@gmail.com>
2023-11-02 23:26:33 +0300

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '88ab697d94mshd044cf405894ec4p10546fjsn4923c6d8b3c4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="9ad1336a393d8da13d0d523979ee1570";
